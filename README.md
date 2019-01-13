5365a9ad1be7550fcb781a39d549cd24f55aceea
Qz <qzcurious@gmail.com>
2019-01-14 00:07:13 +0800

    Update example with line-bot-sdk-php
    Example login user and send a message and an image
    
    line-bot-sdk-php 3.6.0
    SDK should be installed by composer.lock

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Library
 - application/libraries/LINE_login.php
-- application/libraries/LINE_message.php
 
 將上面檔案放到 CI 專案 libraries 資料夾，再在 controller 裡
 用 `$this->load->library('LINE_login', $config)` 來初始化
@@ -13,3 +12,14 @@
 - application/config/line_config.php
 
 在 line_config 的資料為參考用，不要拿去使用喔
+
+# 補充
+[line-bot-sdk-php](https://github.com/line/line-bot-sdk-php)
+使用 composer 下載；在專案資料夾下
+`$ composer require linecorp/line-bot-sdk` 就會安裝到
+*vendor/linecorp/line-bot-sdk/*
+
+在 *config/config.php* 設定
+`$config['composer_autoload'] = 'vendor/autoload.php'`
+就會自動載入 SDK 的 namespace
+
--- application/controllers/Line.php
+++ application/controllers/Line.php
@@ -1,29 +1,57 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// config/config.php 設定
+//   $config['composer_autoload'] = 'vendor/autoload.php';
+
+use LINE\LINEBot;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
 class Line extends CI_Controller {
     public function __construct(){
         parent::__construct();
         $this->config->load('line_config');
-        $auth_info = $this->config->item('auth_info');
-        $this->load->library('LINE_login', $auth_info);
+        $LINE_login_channel_info = $this->config->item('LINE_login_channel_info');
+        $this->load->library('LINE_login', $LINE_login_channel_info);
     }
 
     public function index(){
-        $line_login = $this->config->item('line_login');
-        echo '<a href="'.$this->line_login->authorization_request_string($line_login).'">login</a>';
+        // 產生 LINE 登入連結並顯示於頁面
+        $authorization_param = $this->config->item('LINE_authorization_request_param');
+        echo '<a href="'.$this->line_login->authorization_request_string($authorization_param).'">login</a>';
     }
 
     public function login_callback(){
+        // 接收 line 登入後的結果
         $this->line_login->check_authorization_response();
 
-        $access_token_callback = $this->config->item('line_login')['redirect_uri'];
+        // 為了使用 line login api 的功能，送出 token 請求，這樣才能取得我們需要 userId
+        $access_token_callback = $this->config->item('LINE_authorization_request_param')['redirect_uri'];
         $this->line_login->request_access_token($access_token_callback);
 
+        // 取得 userId
+        // 這邊就可以把 userId 存到資料庫去
         $userId = $this->line_login->get_user_profile()['userId'];
-        $channel_access_token = $this->config->item('channel_access_token');
-        $this->load->library('LINE_message',
-            array('channel_access_token' => $channel_access_token));
-        $this->line_message->push_message($userId, 'Got it!');
+        $channel_access_token = $this->config->item('Messaging_API_info')['channel_access_token'];
+
+        // 以下使用 line php sdk
+        // 建立 LINEBot，要用來傳訊息
+        // 第二個參數必給，但我看不懂，看起來也用不到
+        $bot = new LINEBot(new CurlHTTPClient($channel_access_token), array(
+            'channelSecret' => $this->config->item('Messaging_API_info')['channel_secret']
+        ));
+
+        // 傳送文字訊息
+        // 'SDK message' 改成你要傳的訊息字串
+        $bot->pushMessage($userId, new TextMessageBuilder('SDK message'));
+        // 傳送圖片訊息
+        // ImageMessageBuilder() 兩個參數都要接一個 url
+        //   第一個是真正圖片的 url
+        //   第二個是預覽用的，也就是要縮圖
+        $bot->pushMessage($userId, new ImageMessageBuilder('https://picsum.photos/200/300', 'https://picsum.photos/200/300'));
+
+        echo 'message sent';
     }
 }
