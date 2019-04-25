f22a4da0cb415cd3d5380cf188958f6576328534
Qz <qzcurious@gmail.com>
2019-04-25 13:00:59 +0800

    Update LINE config

--- application/config/line_config.php
+++ application/config/line_config.php
@@ -1,19 +1,19 @@
 <?php
 // LINE login
 $config['LINE_login_channel_info'] = array(
-    'channel_id' => '1636079326',
-    'channel_secret' => 'a9df399e603d4a7f2128846642beee38',
+    'channel_id' => '1568533598',
+    'channel_secret' => 'b30ad652abea5f757adb025988d51bbe',
 );
 
 $config['LINE_authorization_request_param'] = array(
     // make sure this uri is set in App settings of LINE Login channel
     'redirect_uri' => 'https://linebot-qz.herokuapp.com/line/login_callback',
     'scope' => 'profile',
-    'bot_prompt' => 'normal',
+    'bot_prompt' => 'aggressive',
 );
 
 // Message API
 $config['Messaging_API_info'] = array(
-    'channel_secret' => 'e8f51ad15fcc158528e518ce8366113c',
-    'channel_access_token' =>  'ZTqSlVo0UagS+ppQvf1dTOZD7Vi0gDbdMRxw8fqZpug2iSdoKNWMTfhMpy5w76BmmjvZyH8+KUvi41F4s3Zgc87QTZItw3RVn25pRRgHqZuHYBCplYIM042qUJS3x7G52gXvoAI4M6H85POn110H3wdB04t89/1O/w1cDnyilFU=',
+    'channel_secret' => '3a8c1cb03af77effc560228354a716a1',
+    'channel_access_token' =>  'AdBGnxmkALT4Xy4FHH/yz25BwNZCwj2L/I8lLQovC3oLpt9zauS29XDzft3KXit08pLGIU9exskySr9XL5EUxRyVR9FWmrYzNw0E0iL0dIIz7muhKyOIHJHMbIfUEC/bzPgq/U9IwB+8Atx6/xhqqgdB04t89/1O/w1cDnyilFU=',
 );
